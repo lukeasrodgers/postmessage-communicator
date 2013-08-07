4b128e38c1dd07bced279158b66cdf2ba27de32a
Luke Rodgers <lukeasrodgers@gmail.com>
2013-08-07 17:38:51 -0400

    Adding ability to communicate with instantiated object/communicator.

--- test/spec/PostMessageCommunicator_spec.js
+++ test/spec/PostMessageCommunicator_spec.js
@@ -1,4 +1,44 @@
 describe('PostMessageCommunicator', function() {
+  describe('working with an existing remote object', function() {
+    beforeEach(function() {
+      $('body').append('<iframe src="spec/fixtures/iframe_instantiated.html"></iframe>');  
+      this.Constructor = function(sendables) {
+        this.sendables = sendables;
+      };
+      this.Constructor.prototype.submit = function() {};
+    });
+    afterEach(function() {
+      $('iframe').remove();
+    });
+    it('should be able to communicate with an instantiated communicator/sender pair', function() {
+      var sender = new this.Constructor(['remote_submit']);
+      var spy = spyOn(sender, 'submit');
+      var loaded = false;
+      waitsFor(function() {
+        $('iframe').load(function() {
+          loaded = true;
+        });
+        return loaded;
+      }, 'Failed to load iFrame', 500);
+      runs(function() {
+        var communicator = new PostMessageCommunicator({
+          recipient: $('iframe').get(0).contentWindow,
+          sender: sender,
+          target_origin: 'http://localhost'
+        });
+        communicator.set_remote_recipient();
+        waits(50);
+        runs(function() {
+          sender.remote_submit();
+        });
+        waits(50);
+        runs(function() {
+          expect(spy).toHaveBeenCalled();
+          expect(spy).toHaveBeenCalledWith({biz: 'quux'});
+        });
+      });
+    });
+  });
   describe('instantiating a remote object', function() {
     beforeEach(function() {
       $('body').append('<iframe src="spec/fixtures/iframe.html"></iframe>');  
@@ -10,7 +50,7 @@ describe('PostMessageCommunicator', function() {
     afterEach(function() {
       $('iframe').remove();
     });
-    it('should create be able to instantiate an object in another iframe, and exchange messages with it', function() {
+    it('should be able to instantiate an object in another iframe, and exchange messages with it', function() {
       var sender = new this.Constructor(['remote_submit']);
       var spy = spyOn(sender, 'submit');
       var loaded = false;
